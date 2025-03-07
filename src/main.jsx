6e4d017a1bb68f1cd5af0ef0eda893b4f8843555
Reza Buzarjemehri <dev.reza21@gmail.com>
2025-03-07 17:06:50 +0330

    add login route and 404 fallback to main application

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,28 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./components/login/login.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/app" element={<App />} />
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route
+          path="/app"
+          element={<App />}
+        />
+        <Route
+          path="/"
+          element={<h1>Home</h1>}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="*"
+          element={<h1>Not Found</h1>}
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
